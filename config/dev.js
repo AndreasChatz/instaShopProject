680b4c76988b27503509b977debc5b2be9af901d
andreasChatz <andreas.chatzivasileiadis@gmail.com>
2023-05-28 01:36:59 +0300

    Initial setup of Parse Server and Parse Dashboard.

---
+++ config/dev.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+export default {
+  PARSE: {
+    databaseURI: process.env.DB_URI || 'mongodb://localhost:27017/dev',
+    cloud: path.resolve(path.resolve(), './cloud/main.js'),
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    liveQuery: {
+      classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+    },
+  },
+  DASHBOARD: {
+    apps: [
+      {
+        serverURL: process.env.SERVER_URL,
+        appId: process.env.APP_ID || 'myAppId',
+        masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+        appName: process.env.APP_NAME,
+      },
+    ],
+  },
+};
---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { ParseServer } from 'parse-server';
+import ParseDashboard from 'parse-dashboard';
+import path from 'path';
+const __dirname = path.resolve();
+import http from 'http';
+import dotenv from 'dotenv';
+
+import config from './config/index.js';
+
+dotenv.config();
+
+const dashboard = new ParseDashboard(config.DASHBOARD);
+
+export const app = express();
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+if (!process.env.TESTING) {
+  const mountPath = process.env.PARSE_MOUNT || '/parse';
+  const server = new ParseServer(config.PARSE);
+  await server.start();
+  app.use(mountPath, server.app);
+}
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function (req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
+
+if (!process.env.TESTING) {
+  const port = process.env.SERVER_PORT || 1337;
+  const httpServer = http.createServer(app);
+  httpServer.listen(port, function () {
+    console.log('parse-server-example running on port ' + port + '.');
+  });
+  // This will enable the Live Query real-time server
+  await ParseServer.createLiveQueryServer(httpServer);
+}
